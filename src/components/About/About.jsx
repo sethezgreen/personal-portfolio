9645c5f65abcb27a65f818150d365bd131b5acfd
Seth Green <sethezgreen@gmail.com>
2024-06-28 08:35:59 -0400

    updating about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                 <p className="section--title">More</p>
                 <h1 className="section--heading">About Me</h1>
                 <p className="section-description">
-                I am a Full Stack Developer who is eager to improve and always looking for new ways to learn. I studied Biochemistry during my undergraduate program and decided to pursue tech upon graduating. Tech has always been a big part of my personal life, and I am very excited about starting a career in the industry. 
+                I am a Full Stack Developer who is eager to improve and always looking for new ways to learn. I studied Biochemistry during undergrad and decided to pursue tech upon graduating. Tech has always been a big part of my personal life, and I am very excited about starting a career in the industry. 
                 </p>
                 <p className="section-description">
                 I recently finished three full stacks at Coding Dojo; Python, Java, and MERN. Coding has pushed my creativity, problem-solving, and ability to think outside the box. I am currently focused on building more projects and polishing my skills.
